6cf226de21a1681beb28e3c5c3be5f372d1b2c4c
VINITHKUMAR-R <52488869+VINITHKUMAR-R@users.noreply.github.com>
2019-07-11 18:06:39 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,10 +1,2 @@
-s=int(input())
-t=int(input())
-u=int(input())
-if(s>t)and(s>u):
-  print(round(s))
-elif(t>s)and(t>u):
-  print(round(t))
-else(u>s)and(u>t):
-  print(round(u))
- 
+s,t,u=input().split()
+print(max(s,t,u))
