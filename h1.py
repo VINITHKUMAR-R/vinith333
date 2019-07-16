3e347adadb66202d7b8ae7fde7efc0eed117789c
VINITHKUMAR-R <52488869+VINITHKUMAR-R@users.noreply.github.com>
2019-07-16 15:46:15 +0530

    Create h1.py

---
+++ h1.py
@@ -0,0 +1,13 @@
+x=int(input())
+y=list(map(int,input().split()))[:x]
+py=[]
+for i in y:
+  if y.count(i)>1:
+    if str(i) not in py:
+      py.append(str(i))
+  if(py==0):
+    print("unique")
+  else:
+    py.sort()
+print(" ".join(py))
+  
